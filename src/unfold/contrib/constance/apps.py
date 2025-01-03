6681a965e3836a5f415dbf32decde0c1047270ca
rtbrito <rbrito@ubiwhere.com>
2025-01-03 15:54:25 +0000

    fix: change app config name

--- src/unfold/contrib/constance/apps.py
+++ src/unfold/contrib/constance/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SimpleHistoryConfig(AppConfig):
+class ConstanceConfig(AppConfig):
     name = "unfold.contrib.constance"
     label = "unfold_constance"
